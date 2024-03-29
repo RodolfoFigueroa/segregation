82e9475ca18a7cb87d32e1a7723958649fef284f
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2024-03-29 16:24:50 -0600

    Add support for updated metropolitan zones

--- src/segregation/get_met_zones.py
+++ src/segregation/get_met_zones.py
@@ -1,20 +1,40 @@
+import argparse
 import os
 import yaml
-import numpy as np
+
+import geopandas as gpd
 import pandas as pd
 
-sun = pd.read_csv("data/Base_SUN_2018.csv", encoding="Latin-1")
-groups = sun.groupby("CVE_SUN")
-met_zones = {}
 
-for CVE_SUN, df in groups:
-    if not CVE_SUN.startswith("M"):
-        continue
-    CVE_MUN = sorted(df.CVE_MUN.to_list())
-    met_zones[CVE_SUN] = CVE_MUN
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "year",
+        help="Census year.",
+        type=int
+    )
 
-if not os.path.isdir("./output"):
-    os.makedirs("./output")
+    args = parser.parse_args()
 
-with open("./output/met_zones.yaml", "w") as f:
-    yaml.dump(met_zones, f)
+    if args.year == 2018:
+        df = pd.read_csv("./data/BASE_SUN_2018.csv", encoding="latin1")
+
+        df_filtered = df[df["CVE_SUN"].str.startswith("M")].copy()
+        out = df_filtered.groupby("CVE_SUN")["CVE_MUN"].apply(lambda x: list(x))
+
+    elif args.year == 2024:
+        df = gpd.read_file("./data/mpios/mpios_en_metropoli.shp")
+
+        df_filtered = df[df["TIPOMET"] == "Zona metropolitana"].copy()
+        df_filtered["CVEGEO"] = df_filtered["CVEGEO"].astype(int)
+        df_filtered["CVE_ZONA"] = df_filtered["CVE_ZONA"].str.split(".").apply(lambda x: f"M{x[0]}.{x[2]}")
+
+        out = df_filtered.groupby("CVE_ZONA")["CVEGEO"].apply(lambda x: list(x))
+    
+    out = out.to_dict()
+
+    if not os.path.isdir("./output"):
+        os.makedirs("./output")
+
+    with open("./output/met_zones.yaml", "w") as f:
+        yaml.dump(out, f)
